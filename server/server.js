3b2c0d2935131f741d3718e2603c7d3e166da649
Tacs <act.rtacangjr@gmail.com>
2025-04-29 08:40:36 +0800

    feat: Added server boilerplate

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+)
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
