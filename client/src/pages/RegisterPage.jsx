7ddc99cebe986d078dd0e62ac443f533e988601e
Tacs <act.rtacangjr@gmail.com>
2025-05-10 21:37:30 +0800

    refactor: Changed previous codebase for register

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,20 +1,36 @@
+import Form from '../components/Form'
+
 export default function RegisterPage() {
     return (
-        // <form className="w-[350px] flex flex-col p-4 px-8 mt-20 rounded-lg text-white shadow-sm shadow-white ">
-        //     <h2 className="my-4 text-center text-2xl font-bold">Register</h2>
-
-        //     <FieldSet title="Username" type="text" />
-        //     <FieldSet title="Email" type="email" />
-        //     <FieldSet title="Password" type="password" />
-        //     <FieldSet title="Confirm Password" type="password" />
-
-        //     <Button text={'Submit'} />
-
-        //     <p className="text-sm my-2">
-        //         Already have an account?
-        //         <Links path="/auth/login" children={'Login'} />
-        //     </p>
-        // </form>
-        <h2>Register</h2>
+        <Form>
+            <Form.Title title="Register" />
+            <Form.InputField
+                control="username"
+                name="username"
+                label="Username"
+                type="username"
+                placeholder="Enter your username"
+            />
+            <Form.InputField
+                control="email"
+                name="email"
+                label="Email"
+                type="email"
+                placeholder="Enter your email"
+            />
+            <Form.InputField
+                control="password"
+                name="password"
+                label="Password"
+                type="password"
+                placeholder="Enter your password"
+            />
+            <Form.Button text="Submit" />
+            <Form.NavLink
+                text="Already have an account?"
+                path="/auth/login"
+                to="Login"
+            />
+        </Form>
     )
 }
